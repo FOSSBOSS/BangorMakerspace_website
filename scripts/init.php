30f182d93be2bc45d4f08ee18127316fe8566816
FOSSBOSS <blicken@gmail.com>
2025-10-12 19:39:35 -0400

    all pages start the same way

---
+++ scripts/init.php
@@ -0,0 +1,7 @@
+<?php include 'scripts/head.php'; ?>
+  <div class="layout">
+    <div class="sitemap">
+      <?php include 'sitemap.html'; ?>
+    </div>
+    <div class="main">
+	  <?php include 'scripts/header.php'; ?>
